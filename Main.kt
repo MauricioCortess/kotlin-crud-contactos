d58bae8fa3351253cb56adec4d717ef1404bc1d5
MauricioCortess <maucortes2001@gmail.com>
2026-05-26 17:07:51 -0500

    Agregación de funciones de lectura y escritura en archivo de texto.

--- Main.kt
+++ Main.kt
@@ -4,3 +4,19 @@ import java.io.File
 data class Contacto(val id: Int, val nombre: String, val telefono: String)
 
 val archivo = File("contactos.txt")
+
+fun obtenerContactos(): List<Contacto> {
+    if (!archivo.exists()) return emptyList()
+
+    // Colecciones con mapNotNull
+    return archivo.readLines().mapNotNull { Linea ->
+        val datos = linea.split("|")
+        // if para la toma de decisiones
+        if (datos.size == 3) Contacto(datos[0].toInt(), datos[1], datos[2]) else null
+    }
+}
+
+fun guardarContactos(contactos: List<Contacto>) {
+    val contenido = contactos.joinToString("\n") { "${it.id}|${it.nombre}|${it.telefono}" }
+    archivo.writeText(contenido)
+}
